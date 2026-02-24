e9e632a03336c9e8c3e401c3189815773a4364ea
Erich Moenius <162030662+erichmoenius@users.noreply.github.com>
2026-02-24 23:32:26 +0100

    Refactor uniforms in createNebulaMaterial function

--- graphics/NebulaMaterial.js
+++ graphics/NebulaMaterial.js
@@ -6,10 +6,11 @@ export function createNebulaMaterial() {
 
   const uniforms = {
 
-    // Core
-    uTime: { value: 0 },
-    uMouse: { value: new THREE.Vector2(0, 0) },
+    /* ================================
+       CORE
+    ================================= */
 
+    uTime: { value: 0.0 },
 
     uResolution: {
       value: new THREE.Vector2(
@@ -18,21 +19,33 @@ export function createNebulaMaterial() {
       )
     },
 
-    // Scroll States
+    uMouse: {
+      value: new THREE.Vector2(0, 0)
+    },
+
+    /* ================================
+       SCROLL STATES
+    ================================= */
+
     uGas:       { value: 1.0 },
     uWater:     { value: 0.0 },
     uSolid:     { value: 0.0 },
     uFire:      { value: 0.0 },
-    uStillness: { value: 0.0 }
+    uStillness: { value: 0.0 },
+
+    /* ================================
+       AUDIO
+    ================================= */
+
+    uEnergy:    { value: 0.0 }
 
   };
 
   return new THREE.ShaderMaterial({
     vertexShader: vertex,
     fragmentShader: fragment,
-    uniforms,
+    uniforms: uniforms,
     depthTest: false,
     depthWrite: false
   });
-
 }
